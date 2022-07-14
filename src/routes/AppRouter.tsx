e46699a2f983e939da9ea987192ac1c447aad6c0
juand0014 <juand0014@hotmail.com>
2022-07-14 11:20:22 -0400

    route configurated

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,41 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { Fibonacci, Coincidence, GetBanknotes, IsPrime, RandomNumber, Home } from "../pages"
+
+export const mainRoutes = [
+	{
+		path: "/",
+		element: <Home />
+	},
+	{
+		path: "/fibonacci",
+		element: <Fibonacci />
+	},
+	{
+		path: "/concidence",
+		element: <Coincidence />
+	},
+	{
+		path: "/getBanknotes",
+		element: <GetBanknotes />
+	},
+	{
+		path: "/isPrime",
+		element: <IsPrime />
+	},
+	{
+		path: "/randomNumber",
+		element: <RandomNumber />
+	},
+	{
+		path: "/*",
+		element: <Home />
+	}
+]
 
 export const AppRouter = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/concidence" element={<Coincidence />} />
-			<Route path="/getBanknotes" element={<GetBanknotes />} />
-			<Route path="/isPrime" element={<IsPrime />} />
-			<Route path="/randomNumber" element={<RandomNumber />} />
-			<Route path="/fibonacci" element={<Fibonacci />} />
-			<Route path="/*" element={<Home/>}/>
+			{mainRoutes.map((route, index) => <Route key={index} path={route.path} element={route.element} />)}
 		</Routes>
 	)
 }
